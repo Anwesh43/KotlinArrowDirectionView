5365c7a7b4a0abf291d787a34be5846117726969
Anwesh43 <anweshthecool0@gmail.com>
2018-02-27 08:46:36 +0530

    adding State class

--- arrowdirectionview/src/main/java/ui/anwesome/com/arrowdirectionview/ArrowDirectionView.kt
+++ arrowdirectionview/src/main/java/ui/anwesome/com/arrowdirectionview/ArrowDirectionView.kt
@@ -19,4 +19,32 @@ class ArrowDirectionView(ctx : Context) : View(ctx) {
         }
         return true
     }
+    data class State(var prevScale : Float = 0f, var dir : Float = 0f, var jDir : Int = 1, var j : Int = 0) {
+        val scales : Array<Float> = arrayOf(0f, 0f)
+        fun update(stopcb : () -> Unit) {
+            scales[j] += 0.1f * dir
+            if(Math.abs(scales[j] - prevScale) > 1) {
+                scales[j] = prevScale + dir
+                j += jDir
+                if(j == scales.size || j == -1) {
+                    jDir *= -1
+                    j += jDir
+                    prevScale = scales[j]
+                    if(j == 0) {
+                        dir = 0f
+                        stopcb()
+                    }
+                    else {
+                        dir = -1f
+                    }
+                }
+            }
+        }
+        fun startUpdating(startcb : () -> Unit) {
+            if(dir == 0f) {
+                dir = 1f
+                startcb()
+            }
+        }
+    }
 }
