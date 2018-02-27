e36c3d1129930dc99f0dde0836a01d1587814da7
Anwesh43 <anweshthecool0@gmail.com>
2018-02-27 08:39:56 +0530

    adding ArrowDirectionView class

--- arrowdirectionview/src/main/java/ui/anwesome/com/arrowdirectionview/ArrowDirectionView.kt
+++ arrowdirectionview/src/main/java/ui/anwesome/com/arrowdirectionview/ArrowDirectionView.kt
@@ -3,3 +3,20 @@ package ui.anwesome.com.arrowdirectionview
 /**
  * Created by anweshmishra on 27/02/18.
  */
+import android.view.*
+import android.content.*
+import android.graphics.*
+class ArrowDirectionView(ctx : Context) : View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas : Canvas) {
+
+    }
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
