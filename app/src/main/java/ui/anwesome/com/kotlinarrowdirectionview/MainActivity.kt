e1ac075c1fc6145ebf5b4cb0f817214a8b3e061c
Anwesh43 <anweshthecool0@gmail.com>
2018-02-27 09:07:29 +0530

    using ArrowDirectionView in app

--- app/src/main/java/ui/anwesome/com/kotlinarrowdirectionview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinarrowdirectionview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlinarrowdirectionview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.arrowdirectionview.ArrowDirectionView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ArrowDirectionView.create(this)
     }
 }
